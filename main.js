3a494ed4598354d8aea3457424064ec71e4ae58c
Miji Lee <61926294+mijilee1139@users.noreply.github.com>
2021-10-28 20:20:51 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,51 @@
+'use strict'
+
+const items = document.querySelector('.items');
+const form = document.querySelector('.new-form');
+const input = document.querySelector('.footer__input');
+const addBtn = document.querySelector('.footer__button');
+
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    onAdd();
+});
+
+function onAdd() {
+    const text = input.value;
+    if(text === '') {
+        input.focus();
+        return;
+    }
+
+    const item = createItem(text);
+    items.appendChild(item);
+    item.scrollIntoView({block: 'center'});
+    input.value = '';
+    input.focus();
+}
+
+let id=0; 
+function createItem(text) {
+    const itemRow = document.createElement('li');
+    itemRow.setAttribute('class', 'item__row');
+    itemRow.setAttribute('data-id', id);
+    itemRow.innerHTML = `
+        <div class="item">
+            <span class="item__name">${text}</span>
+            <button class="item__delete">
+                <i class="far fa-trash-alt" data-id=${id}></i>
+            </button>
+        </div>
+        <div class="item__divider"></div>
+        `;
+    id++;
+    return itemRow;
+}
+
+items.addEventListener('click', event=> {
+    const id = event.target.dataset.id;
+    if (id) {
+        const toBeDeleted = document.querySelector(`.item__row[data-id="${id}"]`);
+            toBeDeleted.remove();
+    }
+});
